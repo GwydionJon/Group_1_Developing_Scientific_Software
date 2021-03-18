6fcb4ad588238fdb489b96b1524ca67fed7cdb94
Tobias <tobias@kaczun.de>
2021-03-18 15:54:43 +0100

    docstring added, no tests

--- Task2/module_draft/reader.py
+++ Task2/module_draft/reader.py
@@ -10,6 +10,13 @@ import pandas as pd
 class FileReader:
 
     def __init__(self, dir_path):
+        """__init__ Method of FileReader class detects all files in folder 
+        and safes their paths and filenames as dict or does the same for just a 
+        single file given.
+
+        Args:
+            dir_path (str): path to directory or file
+        """
         self.file_dict = {}
 
         if os.path.isdir(dir_path):
@@ -22,6 +29,15 @@ class FileReader:
             self.file_dict[os.path.basename(dir_path)] = dir_path
 
     def read_numpy(self, file_path):
+        """Function to read text files with numpy, transform to complex number
+        and into DataFrame.
+
+        Args:
+            file_path (str): filepath
+
+        Returns:
+            DataFrame: DataFrame from file, dtype=complex and 'time' column
+        """
         temp_np = np.loadtxt(file_path, skiprows=1)
         time = temp_np[:, 0]
         temp_np = temp_np[:, 1:]
@@ -37,10 +53,23 @@ class FileReader:
         return df
 
     def read_pandas_c(self, file_path):
+        """Function to read text files to pandas DataFrame that are whitespace 
+        separated.
+
+        Args:
+            file_path (str): filepath 
+
+        Returns:
+            DataFrame: DataFrame as read from file
+        """
         return pd.read_csv(file_path, delim_whitespace=True)
 
     def read(self):
+        """Fucntion to read all files in Reader Class
 
+        Returns:
+            Dict: Dict containing the DataFrames with filenames as keys
+        """
         self.df_dict = {}
 
         for fn in self.file_dict:
