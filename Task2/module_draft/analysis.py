186de77650e85fcab0da09220ccf4ab5331b7c45
GwydionJon <gwy.daskalakis@googlemail.com>
2021-03-18 16:09:58 +0100

    formatted document

--- Task2/module_draft/analysis.py
+++ Task2/module_draft/analysis.py
@@ -1,13 +1,15 @@
-import numpy as np 
-import pandas as pd 
-import seaborn as sn 
+import numpy as np
+import pandas as pd
+import seaborn as sn
 import matplotlib.pyplot as plt
+
 
 class Analysis:
     """[analysis] parent class for the analysis with variance filter and plot function
 
     """
-    def __init__(self,output_dir):
+
+    def __init__(self, output_dir):
         """[summary]
 
         Args:
@@ -49,8 +51,8 @@ class Analysis:
         else:
             ylabel_list = ylabel
 
-
-        fig, axes = plt.subplots(nr_of_subplots, 1, figsize=(15, 10), sharex=True)
+        fig, axes = plt.subplots(
+            nr_of_subplots, 1, figsize=(15, 10), sharex=True)
 
         for i in range(nr_of_subplots):
             axes[i].plot(df[x_axis].values, df[y_axis_list[i]].values)
@@ -67,6 +69,7 @@ class Statistical_Analysis(Analysis):
     Args:
         Analysis ([dataframe]): dataframe to be analysed    
     """
+
     def __init__(self, output_dir):
         Analysis.__init__(self, output_dir)
 
@@ -76,30 +79,30 @@ class Statistical_Analysis(Analysis):
         Args:
             df ([dataframe): dataframe which we want to plot
         """
-        df=df.drop(df.columns[df.var()<=self.threshold],axis=1)
+        df = df.drop(df.columns[df.var() <= self.threshold], axis=1)
         df.keys()
-        g = sn.relplot(x="time", y="value", hue="variable", kind="line", data=pd.melt(df, ['time']))
+        g = sn.relplot(x="time", y="value", hue="variable",
+                       kind="line", data=pd.melt(df, ['time']))
         g.fig.autofmt_xdate()
 
-
-
-    def correlation(self,df):
+    def correlation(self, df):
         """[Correlation] provides correlation matrix of the dataframe
 
         Args:
             df ([dataframe]): dataframe which we want to compute the correlation from
         """
-        corr_npop = df.corr()   
+        corr_npop = df.corr()
         corr_npop_np = corr_npop.to_numpy()
         corr_npop_np = np.triu(corr_npop_np, k=1)
-        corr_npop_df = pd.DataFrame(data=corr_npop_np, index= ['time', 'MO3', 'MO4', 'MO6', 'MO11', 'MO12', 'MO14'], columns= ['time', 'MO3', 'MO4', 'MO6', 'MO11', 'MO12', 'MO14'])
+        corr_npop_df = pd.DataFrame(data=corr_npop_np, index=['time', 'MO3', 'MO4', 'MO6', 'MO11', 'MO12', 'MO14'], columns=[
+                                    'time', 'MO3', 'MO4', 'MO6', 'MO11', 'MO12', 'MO14'])
         corr_npop_df = corr_npop_df.drop('time', axis=1)
         corr_npop_df = corr_npop_df.melt(ignore_index=False)
-        corr_npop_df = corr_npop_df[corr_npop_df['value']!=0]
-        corr_npop_df = corr_npop_df.sort_values(by='value', key=abs, ascending=False)
+        corr_npop_df = corr_npop_df[corr_npop_df['value'] != 0]
+        corr_npop_df = corr_npop_df.sort_values(
+            by='value', key=abs, ascending=False)
         corr_npop_df
         corr_npop_df.to_csv(self.output_dir+'npop_out.csv')
-
 
     def eucl_distance(self, df):
         """[Euclidean Distance] computes euclidean distance of of the three components
@@ -110,13 +113,13 @@ class Statistical_Analysis(Analysis):
         #table_np = np.loadtxt(filenames_dict["table_dat"], skiprows=1)
         table_np = df.values
         table_np = np.nan_to_num(table_np)
-        dist_2_3 = np.linalg.norm(table_np[:,2]-table_np[:,3])
-        dist_4_5 = np.linalg.norm(table_np[:,4]-table_np[:,5])
-        dist_6_7 = np.linalg.norm(table_np[:,6]-table_np[:,7])
+        dist_2_3 = np.linalg.norm(table_np[:, 2]-table_np[:, 3])
+        dist_4_5 = np.linalg.norm(table_np[:, 4]-table_np[:, 5])
+        dist_6_7 = np.linalg.norm(table_np[:, 6]-table_np[:, 7])
         dist_all = [dist_2_3, dist_4_5, dist_6_7]
         dist_all
         plt.figure()
-        plt.scatter([1, 2, 3], dist_all )
+        plt.scatter([1, 2, 3], dist_all)
         plt.savefig(self.output_dir+'table_plot.pdf')
         plt.show()
         np.savetxt(self.output_dir+'table_out.txt', dist_all)
@@ -135,10 +138,10 @@ class Numerical_Analysis(Analysis):
         Returns:
             [pd.Dataframe]: [with freq and intensity]
         """
-        
+
         rfft = np.abs(np.fft.rfft(df[column_name].values))
         rfft_freq = np.sort(np.fft.fftfreq(rfft.size, step_size))
-        return pd.DataFrame([rfft_freq, rfft],columns=["freq", "intensitys" ])
+        return pd.DataFrame([rfft_freq, rfft], columns=["freq", "intensitys"])
 
     def autocorrelation(self, df, time_label):
         """[calculates the autocorrolation function]
