ed9ae5ce7ee9973205d1c4e6f468d7d31c82db52
GwydionJon <gwy.daskalakis@googlemail.com>
2021-03-18 14:46:48 +0100

    adding some commentary to Analysis class

--- Task2/module_draft/analysis.py
+++ Task2/module_draft/analysis.py
@@ -3,18 +3,38 @@ import pandas as pd
 import seaborn as sn 
 import matplotlib.pyplot as plt
 
-class analysis:
-    """[analysis] parent class for the analysis with variance filter and plot function
+class Analysis:
+    """[summary]
     """
     def __init__(self,output_dir):
+        """[summary]
+
+        Args:
+            output_dir ([string]): set the output dir for the analysis
+        """
         self.threshold = 1e-5
         self.output_dir = output_dir
 
     def remove_low_variance(self, df):
+        """removes all columns with low variance
+
+        Args:
+            df ([pd.Dataframe]): the Dataframe that is to be analyzed
+        """
         df = df.drop(df.columns[df.var() <= self.threshold], axis=1)
 
     def plot_and_save(self, df, x_axis, y_axis, title, xlabel="", ylabel="", nr_of_subplots=1):
-        
+        """plots and saves the given data
+
+        Args:
+            df ([pd.Dataframe]): the Dataframe that is to be analyzed
+            x_axis ([string]): [the column name for the x-axis]
+            y_axis ([string/list]): [the column name(s) for the y-axis]
+            title ([type]): [description]
+            xlabel (str, optional): [description]. Defaults to "".
+            ylabel (str, optional): [description]. Defaults to "".
+            nr_of_subplots (int, optional): [description]. Defaults to 1.
+        """
 
         if(type(y_axis) != list):
             y_axis_list = [y_axis]
@@ -37,5 +57,17 @@ class analysis:
         axes[-1].set_xlabel(xlabel, fontsize = 18)
         fig.savefig(self.output_dir + title + ".pdf")
         plt.show()
+
+
+class Numerical_Analysis(Analysis):
+
+        def fft_with_freq_analysis(self,df,column_name):
+            
+
+
+            
+
+
+
 
 
