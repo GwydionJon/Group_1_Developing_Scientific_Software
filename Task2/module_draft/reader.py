43ab6d16324ab8222dcdf67e03bdd4447a4b65e8
Tobias <tobias@kaczun.de>
2021-03-18 15:26:24 +0100

    adding all fucntions, no doc, no tests

---
+++ Task2/module_draft/reader.py
@@ -0,0 +1,60 @@
+# Author: Tobias Kaczun
+# Date: 18.03.21
+# Package: DSS analysis package
+
+import os
+import numpy as np
+import pandas as pd
+
+
+class FileReader:
+
+    def __init__(self, dir_path):
+        self.file_dict = {}
+
+        if os.path.isdir(dir_path):
+            for path in os.listdir(dir_path):
+                full_path = os.path.join(dir, path)
+                if os.path.isfile(full_path):
+                    self.file_dict[os.path.basename(full_path)] = full_path
+
+        if os.path.isfile(dir_path):
+            self.file_dict[os.path.basename(dir_path)] = dir_path
+
+    def read_numpy(self, file_path):
+        temp_np = np.loadtxt(file_path, skiprows=1)
+        time = temp_np[:, 0]
+        temp_np = temp_np[:, 1:]
+
+        imag_np = np.zeros(
+            (temp_np.shape[0], temp_np.shape[1]/2), dtype=complex)
+        for i in range(imag_np.shape[1]):
+            imag_np[:, i] = temp_np[:, 2*i]+1j*temp_np[:, 2*i+1]
+
+        df = pd.DataFrame(data=imag_np)
+        df['time'] = time
+
+        return df
+
+    def read_pandas_c(self, file_path):
+        return pd.read_csv(file_path, delim_whitespace=True)
+
+    def read(self):
+
+        self.df_dict = {}
+
+        for fn in self.file_dict:
+            file_path = self.file_dict[fn]
+            df = self.read_pandas_c(file_path)
+
+            fline = open("nstate_i.t").readline().rstrip()
+            column = fline.split()
+
+            if len(column) != df.shape[1]:
+                df = self.read_numpy(file_path)
+
+            self.df_dict[fn] = df
+
+        return self.df_dict
+
+
