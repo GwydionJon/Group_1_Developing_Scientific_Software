8cdca36786536559e20e2ada9bd4a8aca88df5e7
GwydionJon <gwy.daskalakis@googlemail.com>
2021-03-22 09:55:01 +0100

    adding first part of numerical analysis

--- Task2/module_draft/main.py
+++ Task2/module_draft/main.py
@@ -40,19 +40,31 @@ def main():
     # add your own args = user_input() for testing and debugging so that you
     # don't have to call the script with full command line input
 
-    args = user_input(['C:\\Users\\windo\\OneDrive\\Dokumente\\Uni\\Sustainable_development_course\\Group_1_Developing_Scientific_Software\\Input\\Task1', '-o',
-    'C:\\Users\\windo\\OneDrive\\Dokumente\\Uni\\Sustainable_development_course\\Group_1_Developing_Scientific_Software\\Output\\Task2\\'])
+    args = user_input(['Input/Task1/', '-o',
+                       'Output/Task2/'])
 
     # read files
     reader = FileReader(args.path)
     input_df = reader.read()
 
     # perform statistical analysis
-    stat_ana = analysis.Statistical_Analysis(args.output)
-    stat_ana.correlation(input_df['npop.t'])
-    stat_ana.eucl_distance(input_df['table.dat'])
+   # stat_ana = analysis.Statistical_Analysis(args.output)
+   # stat_ana.correlation(input_df['npop.t'])
+   # stat_ana.eucl_distance(input_df['table.dat'])
 
     # perfomr numerical analysis
+    num_ana = analysis.Numerical_Analysis(args.output)
+
+    #return new df with the desired columns
+    df_efield_relevant = num_ana.remove_low_variance(input_df['efield.t'])
+
+    #fft with freq of the df
+    df_efield_fft = num_ana.fft_with_freq_analysis(df_efield_relevant, "y")
+
+    #disabled plot to not have it get on my nerves
+    num_ana.plot_and_save(df_efield_fft, "freq", "intensitys",
+                          "efield_fft_analysis", xlabel="Freq",
+                          show_graph=False)
 
 
 if __name__ == "__main__":
