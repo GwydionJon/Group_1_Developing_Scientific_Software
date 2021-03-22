98ea70a4e6e767289c66c13b4cde53331357d646
GwydionJon <gwy.daskalakis@googlemail.com>
2021-03-22 10:24:57 +0100

    adding autocorr for nstate_i

--- Task2/module_draft/main.py
+++ Task2/module_draft/main.py
@@ -48,23 +48,28 @@ def main():
     input_df = reader.read()
 
     # perform statistical analysis
-   # stat_ana = analysis.Statistical_Analysis(args.output)
-   # stat_ana.correlation(input_df['npop.t'])
-   # stat_ana.eucl_distance(input_df['table.dat'])
+    # stat_ana = analysis.Statistical_Analysis(args.output)
+    # stat_ana.correlation(input_df['npop.t'])
+    # stat_ana.eucl_distance(input_df['table.dat'])
 
     # perfomr numerical analysis
     num_ana = analysis.Numerical_Analysis(args.output)
 
-    #return new df with the desired columns
+    # return new df with the desired columns
     df_efield_relevant = num_ana.remove_low_variance(input_df['efield.t'])
 
-    #fft with freq of the df
+    # fft with freq of the df
     df_efield_fft = num_ana.fft_with_freq_analysis(df_efield_relevant, "y")
 
-    #disabled plot to not have it get on my nerves
+    # disabled plot to not have it get on my nerves
     num_ana.plot_and_save(df_efield_fft, "freq", "intensitys",
                           "efield_fft_analysis", xlabel="Freq",
                           show_graph=False)
+
+    df_autocorr = num_ana.autocorrelation(input_df["nstate_i.t"], "time")
+    num_ana.plot_and_save(df_autocorr, "time", ["autocorr_abs", "autocorr_real", "autocorr_imag"],
+                          "nstate_autocorr_analysis", xlabel="time",
+                          show_graph=True)
 
 
 if __name__ == "__main__":
