0d39ad126d05d061d15539331aa704ab4556bf31
Tobias <tobias@kaczun.de>
2021-03-29 19:12:48 +0200

    adding pytest.ini and first test functions

--- Task3/module_draft/test/test_main.py
+++ Task3/module_draft/test/test_main.py
@@ -2,14 +2,19 @@ import pytest
 import os
 import main
 
+
 @pytest.mark.input
 @pytest.mark.parametrize('cmd_string, exp_args',
-    [
-        ('test/', {'path': 'test/', 'output': os.getcwd()}),
-        ('test2/ -o output/', {'path': 'test/', 'output': 'output/'}),
-        ('test2/ --output output/', {'path': 'test/', 'output': 'output/'}),
-        ('test2/ OUTPUT output/', {'path': 'test/', 'output': 'output/'}),
-    ])
+                         [
+                             ('test/', {'path': 'test/',
+                                        'output': os.getcwd()}),
+                             ('test2/ -o output/',
+                              {'path': 'test/', 'output': 'output/'}),
+                             ('test2/ --output output/',
+                              {'path': 'test/', 'output': 'output/'}),
+                             ('test2/ OUTPUT output/',
+                              {'path': 'test/', 'output': 'output/'}),
+                         ])
 def test_user_input(cmd_string, exp_args):
     """Fuction to test user_input() correct assignemnt of command line args.
 
@@ -21,11 +26,12 @@ def test_user_input(cmd_string, exp_args):
     assert main.user_input(sys_argv).path == exp_args['path']
     assert main.user_input(sys_argv).output == exp_args['output']
 
+
 @pytest.mark.input
 @pytest.mark.parametrize('cmd_string, exp_err',
-    [
-        ('', )
-    ])
+                         [
+                             ('', )
+                         ])
 def test_error_user_input(cmd_string, exp_err):
     """Fucntion to test correct Error Messages of user_input.
 
@@ -36,5 +42,3 @@ def test_error_user_input(cmd_string, exp_err):
     sys_argv = cmd_string.split()
     with pytest.raises(exp_err):
         main.user_input(sys_argv)
-
-
--- Task3/module_draft/test/test_reader.py
+++ Task3/module_draft/test/test_reader.py
@@ -1,13 +1,27 @@
 import pytest
 from reader import FileReader
 
-@pytest.fixture()
-def create_temp_dir():
-    
 
-@pytest.mark.Reader
+@pytest.fixture
+def create_temp_dir(tmp_path):
+    # filename and directory constants
+    sub_dir = "sub_dir/"
+    fns = ['file1.txt', 'file2.csv', 'file3.t', 'file4.dat']
+
+    # make sub directory in tmp for files
+    tmp_path.mkdir(sub_dir)
+
+    # create temporary files
+    for fn in fns:
+        fn_path = tmp_path / sub_dir + fn
+        fn_path.touch()
+
+    return tmp_path / sub_dir
+
+
+@pytest.mark.reader
 @pytest.mark.parametrize('dir_path, expected',
-    [
-        (),
-    ])
+                         [
+                             (),
+                         ])
 def test_FileReader(dir_path, expected):
