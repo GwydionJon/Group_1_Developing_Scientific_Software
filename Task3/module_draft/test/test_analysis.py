0e2758aa5535a12cda3e19cecb369135d4844226
GwydionJon <gwy.daskalakis@googlemail.com>
2021-03-31 17:37:42 +0200

    first testing

---
+++ Task3/module_draft/test/test_analysis.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+import analysis
+import reader
+import numpy as np 
+import pandas as pd
+
+
+def get_data_frame_fft():
+    np_array = np.ones(50)
+    my_label = "test_set"
+    df = pd.DataFrame(np_array,columns=my_label)
+    return df, my_label
+
+
+
+def test_fft_with_freq_analysis():
+    assert analysis.fft_with_freq_analysis(get_data_frame_fft(), )
